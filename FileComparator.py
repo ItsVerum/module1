3adbc136ec894e91f11766958ddb9ff1b4c5d136
Veronika Nychyporuk <veronikanychyporuk@gmail.com>
2024-04-05 09:14:37 +0300

    add read_file method

--- FileComparator.py
+++ FileComparator.py
@@ -2,3 +2,7 @@ class FileComparator:
     def __init__(self, file1, file2):
         self.file1 = file1
         self.file2 = file2
+
+    def read_file(self, filename):
+        with open(filename, 'r') as file:
+            return set(line.strip() for line in file)
