4175dcae025016b934e347ff260fee471e36ad98
Veronika Nychyporuk <veronikanychyporuk@gmail.com>
2024-04-05 09:16:33 +0300

    add write_file method

--- FileComparator.py
+++ FileComparator.py
@@ -3,6 +3,13 @@ class FileComparator:
         self.file1 = file1
         self.file2 = file2
 
+
     def read_file(self, filename):
         with open(filename, 'r') as file:
             return set(line.strip() for line in file)
+
+
+    def write_file(self, filename, data):
+        with open(filename, 'w') as file:
+            for line in data:
+                file.write(line + '\n')
