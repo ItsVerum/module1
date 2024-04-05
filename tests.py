b5940b36734be2c7182a0a4d4d67aa820a520cd2
Veronika Nychyporuk <veronikanychyporuk@gmail.com>
2024-04-05 10:05:06 +0300

    add test_compare_files

--- tests.py
+++ tests.py
@@ -12,8 +12,10 @@ def file_comparator(tmpdir):
 
     return FileComparator(str(file1), str(file2))
 
+
 def test_read_file(file_comparator):
     assert file_comparator.read_file(file_comparator.file1) == {"Hello, world!", "Python is great. Yes, it is."}
+
 
 def test_write_file(file_comparator, tmpdir):
     data = {"Hello, world!", "Python is great. Yes, it is."}
@@ -22,3 +24,15 @@ def test_write_file(file_comparator, tmpdir):
     with open(file_path, 'r') as file:
         lines = set(line.strip() for line in file)
     assert lines == data
+
+
+def test_compare_files(file_comparator, tmpdir):
+    same_file = str(tmpdir.join("same.txt"))
+    diff_file = str(tmpdir.join("diff.txt"))
+    file_comparator.compare_files(same_file, diff_file)
+    with open(same_file, 'r') as file:
+        same = set(line.strip() for line in file)
+    with open(diff_file, 'r') as file:
+        diff = set(line.strip() for line in file)
+    assert same == {"Python is great. Yes, it is."}
+    assert diff == {"Hello, world!", "It's a beautiful day."}
